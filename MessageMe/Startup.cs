b7aa8c7678bb14e5f2f4ac53a8551a241047bf9b
Batmobile <Batmobile@DESKTOP-4JR69HN>
2016-10-17 10:16:59 -0500

    Add project files.

---
+++ MessageMe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MessageMe.Startup))]
+namespace MessageMe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
