6a31c19a6396e6a0d260df7d7cfb0dec99f8ec41
Kade <Kadeiszman@gmail.com>
2016-10-25 22:06:37 -0500

    Added an angularjs controller homeApp to the Layout page. Added the HomeController to the Index page

---
+++ MessageMe/AngularController/HomeController.js
@@ -0,0 +1,7 @@
+﻿var homeApp = angular.module('homeApp', []);
+
+
+
+homeApp.controller('homeController', ['$scope', function ($scope) {
+    $scope.names = ['Kirby Pucket', 'Babe Ruth', 'That Yankee guy'];
+}])
