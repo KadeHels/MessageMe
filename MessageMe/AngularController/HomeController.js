f47a0f9aff30ed08050df3934c115228a334339d
Kade <Kadeiszman@gmail.com>
2016-10-31 15:20:07 -0500

    added the api controller

--- MessageMe/AngularController/HomeController.js
+++ MessageMe/AngularController/HomeController.js
@@ -1,6 +1,6 @@
 ﻿var homeApp = angular.module('homeApp', []);
 
-
+//Data connection
 
 homeApp.controller('homeController', ['$scope', function ($scope) {
     $scope.names = ['Kirby Pucket', 'Babe Ruth', 'That Yankee guy'];
